11731a10e6424d84ae3e322fd31e5bee81577864
Tupryk <68149355+Tupryk@users.noreply.github.com>
2023-06-11 16:54:13 +0200

    Update loader.py

--- loader.py
+++ loader.py
@@ -10,7 +10,6 @@ def segment_line(point1, point2, point_between):
     for i in range(point_between):
         new_point = point1 + (point2 - point1) * 0.5 * (1-np.cos(np.pi * i/(point_between-1)))
         points.append(new_point)
-        print(i, new_point)
 
     return points
 
@@ -20,14 +19,25 @@ def load_sketch(file_path, max_dims=[0.5, 0.5], whiteboard_center=[0.25, 1], res
     original_sketch = json.load(open(file_path))
     scaled_sketch = []
 
+    points = []
+    for line in original_sketch:
+        points += line
+    x_coords = [point[0] for point in points]
+    y_coords = [point[1] for point in points]
+    min_x, max_x = min(x_coords), max(x_coords)
+    min_y, max_y = min(y_coords), max(y_coords)
+
     for line in original_sketch:
         scaled_line = []
+
+        scaling_factor_x = max_dims[0] / (max_x - min_x) if max_x != min_x else 1.
+        scaling_factor_y = max_dims[1] / (max_y - min_y) if max_y != min_y else 1.
+
         for point in line:
-            point[0] = (max_dims[0]*point[0]) - (max_dims[0]*0.5)
-            point[1] = (max_dims[1]*point[1]) - (max_dims[1]*0.5)
-            point[0] += whiteboard_center[0] - (max_dims[0]*0.5)
-            point[1] += whiteboard_center[1] - (max_dims[1]*0.5)
+            point[0] = (point[0] - min_x) * scaling_factor_x
+            point[1] = (point[1] - min_y) * scaling_factor_y
             scaled_line.append(point)
+
         scaled_sketch.append(scaled_line)
 
     return scaled_sketch
