e4cf90603dea2c96e6f0de826bdb68a9f11d4160
Eckart Cobo Briesewitz <eck4art@gmail.com>
2023-06-21 07:05:27 +0200

    fix

--- modules/utils.py
+++ modules/utils.py
@@ -4,11 +4,10 @@ from robotic import ry
 
 def sketch_plotter(sketch, ry_config):
 
-    sketch = np.array(sketch)
-    max_x = max(sketch[:, :, 1].flatten())
-    max_y = max(sketch[:, :, 2].flatten())
-    min_x = min(sketch[:, :, 1].flatten())
-    min_y = min(sketch[:, :, 2].flatten())
+    max_x = max(point[1] for line in sketch for point in line)
+    max_y = max(point[2] for line in sketch for point in line)
+    min_x = min(point[1] for line in sketch for point in line)
+    min_y = min(point[2] for line in sketch for point in line)
 
     total_points = 0
     for line in sketch:
@@ -17,7 +16,7 @@ def sketch_plotter(sketch, ry_config):
             ry_config.addFrame("Marker"+str(total_points)) \
                 .setPosition(point) \
                 .setShape(ry.ST.sphere, size=[.005]) \
-                .setColor([255*(point[0]-min_x)/(max_x-min_x), 255*(point[1]-min_y)/(max_y-min_y), 0])
+                .setColor([(point[1]-min_x)/(max_x-min_x), (point[2]-min_y)/(max_y-min_y), 0])
             
             total_points += 1
 
